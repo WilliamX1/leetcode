e4c9cecf1db35c90b01a7524bc989a31ebc36955
WilliamX1 <WilliamX1@users.noreply.github.com>
2021-05-06 08:23:55 +0800

    2021.5.6

---
+++ Solutions/Decode-XORed-Array.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> ans;
+        ans.push_back(first);
+        int n = encoded.size();
+        for (int i = 0; i < n; i++)
+            ans.push_back(encoded[i] ^ ans.back());
+        return ans;
+    }
+};
