53437f96a08173e67d2b135e15ea39e36c35b7ff
WilliamX1 <WilliamX1@users.noreply.github.com>
2021-06-11 17:57:26 +0800

    2021.6.11

---
+++ Solutions/Determine-Whether-Matrix-Can-Be-Obtained-By-Rotation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        int n = mat.size();
+        vector<vector<int>> tmp = mat;
+
+        int maxsize = 4;
+        while (maxsize-- > 0) {
+            bool flag = true;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    tmp[j][n - i - 1] = mat[i][j];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (tmp[i][j] != target[i][j]) {
+                        flag = false;
+                        break;
+                    };
+            if (flag) return true;
+            else mat = tmp;
+        };
+        return false;
+    }
+};
---
+++ Solutions/Reduction-Operations-to-Make-the-Array-Elements-Equal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int size = nums.size();
+        if (size == 1) return 0;
+
+        int ans = 0, cnt = 1;
+        int start = 1;
+        while (start < size && nums[start] == nums[start - 1]) start++;
+        
+        int next_start = start + 1;
+        
+        while (start < size) {
+            while (next_start < size && nums[next_start] == nums[next_start - 1]) next_start++;
+            ans += (next_start - start) * cnt;
+            cnt++;
+            start = next_start;
+            next_start++;
+        };
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> v = {1,1,1};
+    Solution S;
+    std::cout << S.reductionOperations(v);
+    return 0;
+}
