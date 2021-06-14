6f7278e0b695f2cb60a988247cd0eaf01c5303c4
WilliamX1 <WilliamX1@users.noreply.github.com>
2021-06-14 10:11:05 +0800

    2021.6.14

---
+++ Solutions/Guess-Number-Higher-or-Lower.cpp
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low = 1, high = n, mid;
+        while (low <= high) {
+            mid = (low + high) >> 1;
+            switch (guess(mid))
+            {
+            case 0: return mid;
+                break;
+            case 1: low = mid + 1; break;
+            case -1: high = mid - 1; break;
+            default:
+                break;
+            };
+        };
+        return mid;
+    }
+};
