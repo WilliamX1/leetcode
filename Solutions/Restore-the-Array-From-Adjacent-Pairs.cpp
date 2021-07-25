9f8b12166941f1085a3dca5013087b31669da6a4
Xhd <WilliamX1@users.noreply.github.com>
2021-07-25 10:16:14 +0800

    2021.7.25

---
+++ Solutions/Restore-the-Array-From-Adjacent-Pairs.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int, vector<int>> mp;
+        for (auto ele : adjacentPairs)
+        {
+            mp[ele[0]].push_back(ele[1]);
+            mp[ele[1]].push_back(ele[0]);
+        };
+
+        int n = adjacentPairs.size() + 1, begin = 0;
+        vector<int> ans(n);
+        ans[0] = begin, ans[1] = mp[begin][0];
+        for (int i = 2; i < n; i++)
+        {
+            auto adj = mp[ans[i - 1]];
+            ans[i] = ans[i - 2] == adj[0] ? adj[1] : adj[0];
+        };
+        return ans;
+    }
+};
