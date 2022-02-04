e2e02e865e273dd73436172e6fd3368f9e977037
HuiD <391678792hd@sjtu.edu.cn>
2022-02-05 00:12:27 +0800

    2022.2.4

---
+++ Solutions/Number-of-Good-Ways-to-Split-a-String.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSplits(string s) {
+        int n = s.size();
+        set<int> se;
+        vector<int> forward(n, 0);
+        vector<int> backward(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            se.insert(s[i]);
+            forward[i] = se.size();
+        };
+        se.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            se.insert(s[i]);
+            backward[i] = se.size();
+        };
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += forward[i] == backward[i + 1];
+        };
+        return ans;
+    }
+};
---
+++ Solutions/Number-of-Sub-arrays-With-Odd-Sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        int odd = 0, even = 0, mod = 1e9 + 7, tot_odd = 0, tot_even = 0;
+        for (auto num : arr) {
+            int last_odd = odd, last_even = even;
+            if (num & 1) {
+                even = last_odd;
+                odd = last_even + 1;
+            } else {
+                odd = last_odd;
+                even = last_even + 1;
+            };
+            odd %= mod;
+            even %= mod;
+            tot_odd = (tot_odd + odd) % mod;
+        };
+        return tot_odd;
+    }
+};
